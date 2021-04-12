e6c8fbe4f4bcda6795ef172c1d50285f7b17b582
Rheanne Reyes <rheannemr@gmail.com>
2021-04-12 00:29:15 -0700

    Install Mongoose

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +15,7 @@ if (process.env.NODE_ENV === "production") {
 // Define API routes here
 
 // Send every other request to the React app
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
